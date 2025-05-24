4686ea49a37be1a82536e8de125ae009531c2b45
prfsnlprzcovrdsr <arishwaseem05@gmail.com>
2025-05-24 07:28:01 +0000

    Initial commit

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "script/Interactions.s.sol";
+
+contract DeployRaffle is Script {
+    function run() public {}
+
+    function deployContract() public returns (Raffle, HelperConfig) {
+        HelperConfig helperconfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperconfig.getConfig();
+
+        if (config.subscriptionId == 0) {
+            //create subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) = createSubscription
+                .createSubscription(config.vrfCoordinator, config.account);
+
+            //fund it
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                config.vrfCoordinator,
+                config.subscriptionId,
+                config.link,
+                config.account
+            );
+        }
+
+        vm.startBroadcast(config.account);
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        //dont need to broadcast
+        addConsumer.addConsumer(
+            address(raffle),
+            config.vrfCoordinator,
+            config.subscriptionId,
+            config.account
+        );
+
+        return (raffle, helperconfig);
+    }
+}
---
+++ test/integration/Interactions.t.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+//unit
+//integrations
+//forked
+//staging
+
+//fuzzing
+//stateful fuzz
+//stateless fuzz
+//formal verification
